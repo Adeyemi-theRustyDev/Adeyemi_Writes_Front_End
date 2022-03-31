7a774fc3386d7d88044a6d57cfaada089e01387d
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2022-03-31 20:52:40 +0000

    Changed fault to Times New Roman

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,30 @@ const Home = () => {
         <div className="author-info">
           <img src="images/author.jpg" alt="" aria-hidden="true"/>
           <p>
-            My name is Adeyemi. I am an aspirirng writer and poet.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
-            dolores quia fugit odio officiis velit iure sit dolorem rerum
-            cupiditate nisi fugiat perferendis blanditiis similique quae nobis,
-            magnam laudantium id ab repellat esse quo excepturi alias voluptates!
-            Aperiam, placeat eligendi, quia quisquam pariatur saepe aut quam quos
-            quae iure quaerat.
-          </p>
+          Hey there&#x1F44B;, my name is Adeyemi Abiade. I am an aspiring writer
+          and poet.
+        </p>
+        <p>
+          I like to read all sorts of books, especially fantasy, sci-fi and
+          adventure books. <br />
+          I also like challenges and solving problems; in my free time i observe
+          nature and ponder about the mysteries of life. <br />
+          I am currently writing a teen fiction novel, to be published on
+          Wattpad, titled
+          <span
+            ><a href="http://" target="_blank" rel="noopener noreferrer"
+              >Innocuous</a
+            ></span
+          >
+          and also a poetry collection called
+          <span
+            ><a
+              href="https://www.wattpad.com/story/294390680?utm_source=android&amp;utm_medium=link&amp;utm_content=story_info&amp;wp_page=story_details_button&amp;wp_uname=all_about_lichood&amp;wp_originator=cY9zgnf6kbJfJB4gdXiV8BriWcyVrnhr6OW3s2no6djkdiVTpAFE9uI3QmWcLina8OLby%2FF%2BwaogsmbkzXUnSH0f%2F4ynul5KPFAYPDt7MQXIQ%2FR%2FeU8oCQpBw53f3k6a"
+              >Scribbles</a
+            ></span
+          >. <br />
+          I am a Nigerian, Muslim and an Aquarius.
+        </p>
         </div>
         <div className="img-prompt">
           <img src="images/hero-img.jpeg" alt="" />
