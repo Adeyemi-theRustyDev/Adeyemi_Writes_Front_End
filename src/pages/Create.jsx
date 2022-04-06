c4b41e938c24744bea61f2ff0fbba778fe534504
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2022-04-06 17:21:08 +0000

    Styled Create component from

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,24 +1,26 @@
 const Create = () => {
     return ( 
         <div className="create">
-            <form action="">
-                <label htmlFor="title">Title:</label>
-                <input type="text" id="title"/>
-                <label htmlFor="image"></label>
-                <input type="file"  id="image" />
-                <label htmlFor="description">Description:</label>
-                <input type="text" id="description"/>
-                <label htmlFor="category">Category:</label>
-                <input type="text" id="category"/>
-                <label htmlFor="type">Post Type:</label>
-                <select id="type">
-                    <option value="poem">Poem</option>
-                    <option value="blog post">Blog post</option>
-                    <option value="story">Story</option>
-                </select>
-                <label htmlFor="body">Body:</label>
-                <textarea id="body"></textarea>
-            </form>
+            <div className="create-form">
+                <form action="">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title"/>
+                    <label htmlFor="image">Image</label>
+                    <input type="file"  id="image" />
+                    <label htmlFor="description">Description:</label>
+                    <input type="text" id="description"/>
+                    <label htmlFor="category">Category:</label>
+                    <input type="text" id="category"/>
+                    <label htmlFor="type">Post Type:</label>
+                    <select id="type">
+                        <option value="poem">Poem</option>
+                        <option value="blog post">Blog post</option>
+                        <option value="story">Story</option>
+                    </select>
+                    <label htmlFor="body">Body:</label>
+                    <textarea id="body"></textarea>
+                </form>
+            </div>
         </div>
      );
 }
