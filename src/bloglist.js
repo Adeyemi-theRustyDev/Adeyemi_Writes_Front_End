4c87ad81a905034f72585dae489664aafd176906
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2022-04-06 12:20:06 +0000

    Working with json-server

---
+++ src/bloglist.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom';
+const Bloglist = ({blogs}) => {
+    return ( 
+        <div className="bloglist">
+            {blogs.map((poem)=>(
+                    <div className="poem-details" key={poem.id}>
+                        <h1>{poem.title}</h1>
+                        <img src={poem.imageUrl} alt=""/>
+                        <Link to={poem.imageUrl} target="_blank" rel="noreferrer" ><p>{poem.body}</p></Link>
+                        <p>Created at {poem.created}</p>
+                    </div>
+                ))}
+        </div>
+     );
+}
+ 
+export default Bloglist;
---
+++ src/poemadvanced.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import Bloglist from '../bloglist';
+const Poems = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=> {
+        fetch('http://localhost:8005/posts')
+        .then(res => {
+          return  res.json();
+        })
+        .then((data) => {
+            setBlogs(data);
+            setIsPending(false);
+        })
+        .catch((err) =>{
+            setError(true);
+        })
+    }, [blogs]);
+    return ( 
+        <div className="poem-component">
+            <h1 className="poem-component-title">Poem Library</h1>
+            <div className="search">           
+                <input type="search" name="" id="" placeholder="Search through poems..." />
+            </div>
+
+            <div className="poem-map">
+               {blogs && <Bloglist blogs={blogs.filter((blog)=> blog.type === "poem")}/>}
+            </div>
+        </div>
+        
+     );
+}
+ 
+export default Poems;
