c7b4ea9dc3ae925610bfb4ccf06b5eb0c46ecf65
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2022-04-14 21:00:14 +0000

    Fixed font issues

---
+++ src/components/DetailedPoem.jsx
@@ -0,0 +1,15 @@
+const DetailedPoem = () => {
+    return ( 
+        <div className="poem-details">
+            <div className="poem-image">
+                <img src="" alt="" />
+                <div className="description"></div>
+            </div>
+            <div className="poem-text">
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default DetailedPoem;
